34212eb1e662994b0e50edbe52ae82e67a2932b9
davidchampredon <david.champredon@gmail.com>
2018-01-10 09:39:50 -0500

    add script to export to live Github repo

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # seminribm-dev
 Development for seminribm
 
-Run `build_library` to build and install (locally) the `seminribm` package.
+Run `build_library.sh` to build and install (locally) the `seminribm` package.
+
+Run `export-to-pckg-repo.sh` to export the selected files to the _live_ Github repository for this package. 
+
---
+++ export-to-pckg-repo.sh
@@ -0,0 +1,13 @@
+### Export to LIVE package Github repository
+### Does NOT export the DESCRIPTION, NAMESPACE and LICENSE files
+
+### R files:
+cp seminribm/R/*.R ../seminribm/R
+
+### C++ source files:
+cp seminribm/src/* ../seminribm/src
+
+### Documentation:
+cp seminribm/man/* ../seminribm/man
+cp vignettes/*.Rmd ../seminribm/vignettes
+
